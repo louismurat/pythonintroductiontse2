e0f8e2cdb06f2d07b99fba5d1a64a5f6aa61663a
louismurat <lmurat@laposte.net>
2016-12-10 16:03:23 +0100

    exo tentative

---
+++ MyModule.py
@@ -0,0 +1,5 @@
+class Animal(object):
+    def _init_(self, name):
+        self.name = name
+        def printName(self):
+            print(self.name)
--- git file.py
+++ git file.py
@@ -1,2 +1,22 @@
-print("hello ")
-print (" i was wondering if after all this year you would like to meet i have ")
+# import pandas as pd
+# s = pd.Series([1, 3, 5])
+# print(s)
+#
+# from MyModule import Animal
+# lion = Animal("georges")
+# lion.printName()
+
+
+#ex
+
+med={1,3,8,10}
+
+
+def median(lst):
+    lst = sort(lst)
+    if len(lst) % 2 == 1:
+            return lst[((len(lst)+1)/2)-1]
+    else:
+            return lst[((len(lst)+1)/2)]
+
+print(median(med))
